8e94ce107b55e6028c5724f62ef28a52f92304e1
Jiacch <jiaccoding@163.com>
2017-09-22 09:53:05 +0800

    add DFS

---
+++ No.1/DFS_POJ2386.py
@@ -0,0 +1,47 @@
+# -*- coding:UTF-8 -*-
+
+# Lake Counting
+
+# data input
+m = 12
+n = 10
+input_arr = [
+    'W........WW.',
+    '.WWW.....WWW',
+    '....WW...WW.',
+    '.........WW.',
+    '.........W..',
+    '..W......W..',
+    '.W.W.....WW.',
+    'W.W.W.....W.',
+    '.W.W......W.',
+    '..W.......W.'
+    ]
+arr = [ [] for i in range(len(input_arr))]
+for i in range(len(input_arr)):
+    for j in range(len(input_arr[i])):
+        arr[i].append(str(input_arr[i][j]))
+
+def DFS(i, j):
+    arr[i][j] = '.'
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            newx = i + dx
+            newy = j + dy
+            if newx>=0 and newy>=0 and newx<n and newy<m and arr[newx][newy]=='W':
+                DFS(newx, newy)
+    return
+    pass
+
+def main():
+    count = 0
+    for x in range(0,n-1):
+        for y in range(0, m-1):
+            if arr[x][y] == 'W':
+                DFS(x, y)
+                count += 1
+    print count
+    pass
+
+if __name__ == '__main__':
+    main()
